6cc820729a8ea07fc6cff0a3c2d42f84117d6564
ameeenamzz <shoukethameen@gmail.com>
2025-07-13 08:13:06 +0530

    feat: add axios dependency, implement login form with state management

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  return (
+    <div className="flex justify-center my-24">
+      <fieldset className="fieldset bg-base-300 border-base-300 rounded-box w-xs border p-4">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input
+          type="email"
+          value={email}
+          className="input"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label className="label">Password</label>
+        <input
+          type="password"
+          value={password}
+          className="input"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="btn btn-neutral mt-4">Login</button>
+      </fieldset>
+    </div>
+  );
+};
+
+export default Login;
